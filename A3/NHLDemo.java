a82e37b3472ced3e75632fb958313dfce17e345b
mp-school <mg770448@dal.ca>
2024-10-09 14:25:12 -0300

    L:A3 - need to fix try catch error

--- A3/NHLDemo.java
+++ A3/NHLDemo.java
@@ -3,6 +3,8 @@ import java.util.*;
 import java.io.File;
 import java.util.Scanner; 
 import java.io.FileNotFoundException;
+import java.io.FileWriter;   
+import java.io.IOException;
 
 
 public class NHLDemo {
@@ -17,31 +19,38 @@ Scanner in = new Scanner(System.in);
 
 String filein = in.nextLine();
 
-    try{
-        File reader = new File(filein);
-        Scanner n = new Scanner(reader);
-
-        while (n.hasNextLine()) {
-          String data = n.nextLine();
-          System.out.println(data);
-        }
-
-        }catch (FileNotFoundException e) {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
     
-        
+     File reader = new File(filein);
+     Scanner n = new Scanner(reader);
+
+
+    
+
+    
+    try {
+        while (n.hasNextLine()) {
+  
+            // writes to ouput file
+            FileWriter myWriter = new FileWriter("filename.txt");
+            myWriter.write("Files in Java might be tricky, but it is fun enough!");
+            myWriter.close();
         }
-}
+    } catch (IOException e) {
+        System.out.println("An error occurred.");
+        e.printStackTrace();
+    }
 
-
-
-
-
-NHLStats obj = new NHLStats();
-
-
+     }
+    NHLStats obj = new NHLStats();
+        
+    
 
 
     }
+
+
+
+
+    
+
 
--- A3/looseCode.java
+++ A3/looseCode.java
@@ -9,21 +9,7 @@ public class looseCode {
     // prints file
     public static void main(String[] args){
         
-        try{
-        File reader = new File("/Users/meganpicard/Documents/CSCI-2110/A3/nhlstats.txt");
-        Scanner in = new Scanner(reader);
-
-        while (in.hasNextLine()) {
-          String data = in.nextLine();
-          System.out.println(data);
-        }
-
-        }catch (FileNotFoundException e) {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
-    
-        
-        }
+       
 
 
 

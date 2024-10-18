a338a3bd179e4972b2fa1a4c95030af11026befa
Megan112004 <megpicard04@gmail.com>
2024-10-18 10:30:17 -0300

    MS:Lab5 - Even and Odd methods Functional

--- Labs/Lab5/LLDemo.java
+++ Labs/Lab5/LLDemo.java
@@ -38,24 +38,21 @@ LinkedList<String> pokemon = new LinkedList<String>();
     
 
 // Even Indices
-public static void displayOdd(LinkedList<String> list){
+public static void displayEven(LinkedList<String> list){
 
-    for(int i = 0; i < list.size(); i++){
-        if(i % 2 != 0){
-            System.out.print(list.getAt(i) + " ");
-            }
+    for(int i = 0; i < list.size(); i += 2){
+        System.out.print(list.getAt(i) + " ");
     }
-}
+} 
 
 
 
 // Odd Indices
-public static void displayEven(LinkedList<String> list){
+public static void displayOdd(LinkedList<String> list){
 
-    for(int i = 0; i < list.size(); i++){
-        if(i % 2 == 0 || i == 0){
+    for(int i = 1; i < list.size();  i += 2){
             System.out.print(list.getAt(i) + " ");
-        }
+
     }
 }
 
@@ -91,14 +88,20 @@ Sydney
 quit
 
 
-0
-1
-2
-3
-4
-5
-6
+13
+12
+11
+10
+9
+8
 7
+6
+5
+4
+3
+2
+1
+0
 quit
  * 
  * 

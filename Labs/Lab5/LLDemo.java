b53ab08a50bb0a638a406a7cb95078381badb994
mp-school <mg770448@dal.ca>
2024-10-18 10:14:14 -0300

    L:LAb5 - TestCases & Even and odd(kind of working) methods

--- Labs/Lab5/LLDemo.java
+++ Labs/Lab5/LLDemo.java
@@ -27,7 +27,79 @@ LinkedList<String> pokemon = new LinkedList<String>();
         System.out.println("\nPokemon in the list:");
         pokemon.enumerate();
 
+        System.out.println("\n Even Pokemon in the list:");
+        displayEven(pokemon);
+
+        System.out.println("\n Odd Pokemon in the list:");
+        displayOdd(pokemon);
 
     }
 
+    
+
+// Even Indices
+public static void displayOdd(LinkedList<String> list){
+
+    for(int i = 0; i < list.size(); i++){
+        if(i % 2 != 0){
+            System.out.print(list.getAt(i) + " ");
+            }
+    }
 }
+
+
+
+// Odd Indices
+public static void displayEven(LinkedList<String> list){
+
+    for(int i = 0; i < list.size(); i++){
+        if(i % 2 == 0 || i == 0){
+            System.out.print(list.getAt(i) + " ");
+        }
+    }
+}
+
+
+
+}
+
+
+// Test Cases
+/*
+
+Pikachu
+Charizard
+Mewtwo
+Emboar
+Bulbasaur
+Eevee
+Gengar
+Ditto
+Charmander
+quit
+
+
+
+Keko
+Mika
+Crumb
+Pookie
+SnowGlobe
+Peri
+Pablo
+Sydney
+quit
+
+
+0
+1
+2
+3
+4
+5
+6
+7
+quit
+ * 
+ * 
+ */

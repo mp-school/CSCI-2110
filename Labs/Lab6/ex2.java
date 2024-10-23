0cf44b2a98f125eaaf4d85642d164c7c4c8a35b3
mp-school <mg770448@dal.ca>
2024-10-23 20:50:23 -0300

    L:Lab5 - Finished Ex2

---
+++ Labs/Lab6/ex2.java
@@ -0,0 +1,34 @@
+package Labs.Lab6;
+// Megan Picard
+// B009393548
+
+
+import java.util.*;
+
+
+
+public class ex2 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print( "Enter a positive integer: ");
+        int n = in.nextInt();
+        countDown(n);
+
+    }
+
+    public static void countDown(int n){
+        if (n == 0) {
+            System.out.println("BlastOff!");
+        } else {
+            System.out.print(n + "   ");
+            countDown(n - 1);
+        }
+    }
+
+
+
+
+
+
+}

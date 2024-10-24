923fc42f076e0402874dbc8668e8bc10d9687be8
mp-school <mg770448@dal.ca>
2024-10-23 22:53:41 -0300

    L:LAb6 - Fixed ex4 output + added test case ouput to txt file

--- Labs/Lab6/ex4.java
+++ Labs/Lab6/ex4.java
@@ -23,17 +23,15 @@ public class ex4 {
 
     public static void multiples(int n, int m){
         
-        if (m == 0){ // base
-            return;
+        
+        if (m == 1){ // base
+            System.out.print(n * m + " ");
         } else {
+            System.out.print(n * m + ", ");
             multiples(n, m - 1); // glue
         }
 
-        if (m >= 1){
-            System.out.print(n * m + ", ");
-        } else {
-            System.out.print(n * m);
-        }
+       
             }
         }
 
